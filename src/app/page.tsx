e32194b9fee0126fb09451b9232c936b0dbff430
0yassin <yassinz8275@gmail.com>
2025-09-08 19:36:18 +0100

    added section 5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
           <a className="self-center font-normal text-black/90 text-2xl">logo</a>
           <button className="border-1 relative group inline-flex border-black/60 py-4 px-9 rounded-full text-[15px] text-black/90 font-normal cursor-pointer">
             <span className="group-hover:-translate-x-3 transition-all">contact us</span>
-            <span className="absolute right-6 translate-x-3 opacity-0 group-hover:-translate-x-0 group-hover:opacity-100 transition-all">A</span>
+            <img src={"/downarrow.svg"} className="absolute -rotate-90 right-6 translate-x-3 opacity-0 group-hover:-translate-x-0 group-hover:opacity-100 transition-all w-4 inline-block self-center"/>
           </button>
        </nav>
 
@@ -24,7 +24,7 @@ export default function Home() {
           
           <button className="border-1 group h-fit w-fit self-center relative  border-black/60 py-5 px-12 rounded-full text-[25px] text-black/90 font-normal cursor-pointer inline-flex">
             <span className="group-hover:-translate-x-4 transition-all">book a call</span>
-            <span className="absolute right-6 translate-x-4 opacity-0 transition-all group-hover:translate-0 group-hover:opacity-100">A</span>
+            <img src={"/downarrow.svg"} className="inline-block w-6 self-center absolute right-6 translate-x-4 opacity-0 transition-all group-hover:translate-0 group-hover:opacity-100 -rotate-90"/>
           </button>
           {/* <a className="border-1 h-fit w-fit self-center  border-black/60 py-5 px-12 rounded-full text-[25px] text-black/90 font-normal">book a call</a> */}
         </div>
@@ -80,11 +80,11 @@ export default function Home() {
       </section>
 
       {/* section 3 */}
-      <section className="mb-64 w-full py-8 max-w-[950px]">
+      <section className="w-full mt-16 max-w-[950px]">
       <h1 className="text-[40px] font-medium text-black/100 mb-8 w-fit">
         Our services
       </h1>
-      <div className="w-fit py-2 px-2 rounded-2xl border-[1px] border-black/70 flex gap-2 mx-auto">
+      <div className="w-fit py-2 px-2 rounded-2xl border-[1.5px] border-black/70 flex gap-2 mx-auto">
 
         <div className="bg-[#303030] py-16 px-16 rounded-[8px] flex justify-center">
           <span className="text-[25px] font-normal text-white">Branding</span>
@@ -95,11 +95,50 @@ export default function Home() {
         <div className="bg-[#303030] py-16 px-16 rounded-[8px] flex justify-center">
           <span className="text-[25px] font-normal text-white">Branding</span>
         </div>
-        <img src={"/downarrow.svg"} className="w-12 mr-2 -rotate-90"></img>
-
+        <img src={"/downarrow.svg"} className="w-10 mr-2 -rotate-90"></img>
 
       </div>
       </section>
+
+      {/* section 4 */}
+      <section className="w-full mt-16 max-w-[950px]">
+      <h1 className="text-[40px] font-medium text-black/100 mb-2 w-fit">
+        About us
+      </h1>
+      <p className="text-[25px] text-black/90">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+et dolore magna aliqua Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
+et dolore magna aliqua. 
+      </p>
+      </section>
+
+      {/* section 5 */}
+      <section className="w-full mt-8 max-w-[950px] mb-32">
+      <h1 className="text-[40px] font-medium text-black/100 w-fit mb-12">
+        Recent Projects
+      </h1>
+
+      {/* image grid */}
+      <div className="grid grid-cols-3 grid-rows-1 max-w-[700px] gap-2 mx-auto">
+
+        <div className="col-span-2 row-span-1">
+          <img src={"/image 1.png"} className="object-cover w-full h-full"></img>
+        </div>
+
+        <div className="col-span-1 row-span-1">
+          <img src={"/image 1.png"} className="w-full h-full object-cover rounded-xl object-left"/>
+        </div>
+
+        <div className="col-span-1 row-span-1">
+          <img src={"/image 1.png"} className="w-full h-full object-cover rounded-xl object-left"/>
+        </div>
+
+        <div className="col-span-2 row-span-1">
+          <img src={"/image 1.png"} className="w-full h-full object-cover rounded-xl object-left"/>
+        </div>
+      </div>
+      </section>
+
     </div>
   );
 }
